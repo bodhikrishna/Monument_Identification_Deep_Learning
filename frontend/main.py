d0ce242ef01c4121b05ae2a3886f096bef07aeb7
ggmxip <92012598+ggmxip@users.noreply.github.com>
2023-06-24 23:46:07 +0530

    rename

--- frontend/main.py
+++ frontend/main.py
@@ -15,7 +15,7 @@ from PIL import Image
 
 
 # importing css file to add more css to existing frontend
-with open("design.css") as source_des:
+with open("frontend/design.css") as source_des:
     st.markdown(f"<style>{source_des.read()}</style>", unsafe_allow_html=True)
 st.markdown("![Alt Text](https://media.giphy.com/media/doXBzUFJRxpaUbuaqz/giphy.gif)")
 st.set_option("deprecation.showfileUploaderEncoding", False)
